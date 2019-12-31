e7d041834c730322acd037c27ce5292e2a6e5e31
Zachary <zachary.zboncak@gmail.com>
2019-12-31 03:22:19 -0600

    added all route handlers

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('../logger');
 const bookmarks = require('../store');
+const uuid = require('uuid/v4');
 
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
@@ -10,5 +11,77 @@ bookmarksRouter
     .get((req, res) => {
         res.json(bookmarks);
     })
+    .post(bodyParser, (req, res) => {
+        const { title, url, rating=3, description="Add description here"} = req.body;
+
+        if(!title) {
+            logger.error('Title is required');
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        if(!url) {
+            logger.error('URL is required');
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        const id = uuid();
+
+        const bookmark = {
+            id,
+            title,
+            url,
+            rating,
+            description
+        };
+
+        bookmarks.push(bookmark);
+
+        logger.info(`Bookmark with id ${id} created`);
+
+        res
+            .send(201)
+            .location(`http://localhost:8000/bookmarks/${id}`)
+            .json(bookmark);
+    })
+
+bookmarksRouter
+    .route('/bookmarks/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        const bookmark = bookmarks.find(bkmk => bkmk.id == id);
+
+        if(!bookmark) {
+            logger.error(`Bookmark with id ${id} not found.`);
+            return res
+                .status(404)
+                .send('Bookmark not found');
+        }
+
+        res.json(bookmark);
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+
+        const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+
+        if (bookmarkIndex === -1) {
+            logger.error(`Bookmark with id ${id} not found`);
+            return res
+                .status(404)
+                .send('Not found');
+        }
+
+        bookmarks.splice(bookmarkIndex, 1);
+
+        logger.info(`Bookmark with id ${id} deleted.`);
+
+        res
+            .status(204)
+            .end();
+    });
 
 module.exports = bookmarksRouter;
--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 const bookmarks = [
     {
+    id: 1,
     title:"Google",
     url:"http://www.google.com", 
     rating:"3", 
     description:"No evil"
     },
     {
+      id: 2,
       title:"Yahoo",
       url:"http://www.yahoo.com", 
       rating:"4", 
