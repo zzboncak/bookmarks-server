86891116f97e70c6a2c9205729bf8c7c7b521e42
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-01-11 13:36:02 -0600

    set up first migrations and seeded data

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,13 @@
+insert into bookmarks (title, url, description, rating)
+values
+  ('Bookmark 1', 'www.website1.com', 'just a website', 4 ),
+  ('Bookmark 2', 'www.website2.com', null, 2 ),
+  ('Bookmark 3', 'www.website3.com', 'just another website', 3),
+  ('Bookmark 4', 'www.website4.com', 'another another website', 3),
+  ('Bookmark 5', 'www.website5.com', null, 6 ),
+  ('Bookmark 6', 'www.website6.com', null, 4),
+  ('Bookmark 7', 'www.website7.com', 'website asdfasdf', 4),
+  ('Bookmark 8', 'www.website8.com', 'so many websites', 3),
+  ('Bookmark 9', 'www.website9.com', 'more websites', 1),
+  ('Bookmark 10', 'www.website10.com', null, 5),
+  ('Bookmark 11', 'www.website11.com', 'all the websites', 10);
